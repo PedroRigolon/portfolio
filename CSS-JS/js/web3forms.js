f449dd7e25a4c84b9c924298ea67ac422552c9ce
Pedro Rigolon <pedrorigoloncaram@gmail.com.com>
2025-03-06 03:02:54 -0300

    site desminificado. Adicionado: contato funcionando, js mudado para arquivo para diminuir trabalho. Otimizacao do codigo.

--- CSS-JS/js/web3forms.js
+++ CSS-JS/js/web3forms.js
@@ -1,2 +1,43 @@
-const form=document.getElementById('form');const result=document.getElementById('result');form.addEventListener('submit',async function(e){e.preventDefault();const hCaptcha=form.querySelector('textarea[name=h-captcha-response]').value;if(!hCaptcha){showResult("Por favor, preencha o campo do captcha.",'red');return}
-try{const formData=new FormData(form);const response=await fetch('https://api.web3forms.com/submit',{method:'POST',body:formData,});const data=await response.json();const message=data.success?"Mensagem enviada com sucesso!":"Ocorreu um erro ao enviar a mensagem.";showResult(message,data.success?'green':'red')}catch(error){showResult("Ocorreu um erro ao enviar a mensagem.",'red')}});function showResult(message,color){result.innerHTML=`<p style="color: ${color}; font-weight: 500; margin-top: 20px;">${message}</p>`;setTimeout(()=>{result.innerHTML=''},8000)}
+const form = document.getElementById('form');
+const result = document.getElementById('result');
+
+form.addEventListener('submit', async function (e) {
+  e.preventDefault();
+
+  
+  const hCaptcha = form.querySelector('textarea[name=h-captcha-response]').value;
+
+ 
+  if (!hCaptcha) {
+    showResult("Por favor, preencha o campo do captcha.", 'red');
+    return;
+  }
+
+  
+  try {
+    const formData = new FormData(form);
+    const response = await fetch('https://api.web3forms.com/submit', {
+      method: 'POST',
+      body: formData,
+    });
+
+    const data = await response.json();
+    const message = data.success 
+      ? "Mensagem enviada com sucesso!" 
+      : "Ocorreu um erro ao enviar a mensagem.";
+
+    
+    showResult(message, data.success ? 'green' : 'red');
+  } catch (error) {
+    
+    showResult("Ocorreu um erro ao enviar a mensagem.", 'red');
+  }
+});
+
+
+function showResult(message, color) {
+  result.innerHTML = `<p style="color: ${color}; font-weight: 500; margin-top: 20px;">${message}</p>`;
+  setTimeout(() => {
+    result.innerHTML = '';
+  }, 8000);
+}
