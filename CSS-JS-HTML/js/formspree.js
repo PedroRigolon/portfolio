52b1b6d65871175da75be65a3860df8f2873372b
Pedro Rigolon <pedrorigoloncaram@gmail.com.com>
2025-03-09 17:18:56 -0300

    adicionado: mensagem de feedback de envio de formulario melhorada.

--- CSS-JS-HTML/js/formspree.js
+++ CSS-JS-HTML/js/formspree.js
@@ -1,30 +1,44 @@
 var form = document.getElementById("my-form");
-  
-  async function handleSubmit(event) {
+
+async function handleSubmit(event) {
     event.preventDefault();
     var status = document.getElementById("my-form-status");
     var data = new FormData(event.target);
+
     fetch(event.target.action, {
-      method: form.method,
-      body: data,
-      headers: {
-          'Accept': 'application/json'
-      }
+        method: form.method,
+        body: data,
+        headers: {
+            'Accept': 'application/json'
+        }
     }).then(response => {
-      if (response.ok) {
-        status.innerHTML = "Mensagem enviada!";
-        form.reset()
-      } else {
-        response.json().then(data => {
-          if (Object.hasOwn(data, 'errors')) {
-            status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
-          } else {
-            status.innerHTML = "Ocorreu um erro ao enviar a mensagem!"
-          }
-        })
-      }
+        if (response.ok) {
+            status.innerHTML = "Mensagem enviada!";
+            status.classList.remove("hidden");  
+            status.classList.add("visible");    
+            form.reset();
+
+            setTimeout(() => {
+                status.classList.remove("visible");
+                status.classList.add("hidden");
+            }, 5000);
+
+        } else {
+            response.json().then(data => {
+                if (Object.hasOwn(data, 'errors')) {
+                    status.innerHTML = data["errors"].map(error => error["message"]).join(", ");
+                } else {
+                    status.innerHTML = "Ocorreu um erro ao enviar a mensagem!";
+                }
+                status.classList.remove("hidden");
+                status.classList.add("visible");
+            })
+        }
     }).catch(error => {
-      status.innerHTML = "Ocorreu um erro ao enviar a mensagem!"
+        status.innerHTML = "Ocorreu um erro ao enviar a mensagem!";
+        status.classList.remove("hidden");
+        status.classList.add("visible");
     });
-  }
-  form.addEventListener("submit", handleSubmit)
+}
+
+form.addEventListener("submit", handleSubmit);
