82e382b4030f94001dcdc8db06d1a947b715573d
Pedro Rigolon <pedrorigoloncaram@gmail.com.com>
2025-03-11 02:14:57 -0300

    adicionado: modal nas paginas de certificados (apenas na maua por enquanto). CRIAR UM JS SEPARADO PARA O MODAL E ATUALIZAR NAS PAGINAS RESTANTES. Diretorio dos arquivos arrumado (apenas na pagina da maua)

--- CSS-JS-HTML/js/botaoDireito.js
+++ CSS-JS-HTML/js/botaoDireito.js
@@ -1,3 +1,10 @@
 document.addEventListener("contextmenu", function(event) {
     event.preventDefault(); // Impede o menu de contexto de aparecer
   });
+
+  document.querySelectorAll('.open-modal').forEach(button => {
+    button.addEventListener('click', function() {
+        const imageUrl = this.getAttribute('href'); // Pega o href do botão clicado
+        document.getElementById('modalImage').setAttribute('src', imageUrl); // Define a imagem no modal
+    });
+});
